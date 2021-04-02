79198308475afa68dbcddc3ff3d082d4aad5f143
KQTENQK <67883180+KQTENQK@users.noreply.github.com>
2021-04-02 19:42:45 +0300

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _enemySpawners;
+    [SerializeField] private GameObject[] _enemySpawnPoints;
     [SerializeField] private GameObject _prefabEnemy;
     [SerializeField] private int _enemyCount;
+    [SerializeField] private float _spawnDelay;
 
     private IEnumerator SpawnEnemy()
     {
         int spawnerIndex = 0;
         for(int i = 0; i < _enemyCount; i++, spawnerIndex++)
         {
-            if (spawnerIndex > _enemySpawners.Length - 1)
+            if (spawnerIndex > _enemySpawnPoints.Length - 1)
                 spawnerIndex = 0;
 
-            Instantiate(_prefabEnemy, _enemySpawners[spawnerIndex].transform.position, Quaternion.identity);
+            Instantiate(_prefabEnemy, _enemySpawnPoints[spawnerIndex].transform.position, Quaternion.identity);
             
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(_spawnDelay);
         }
     }
 
